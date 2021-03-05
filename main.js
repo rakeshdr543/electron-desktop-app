428d25f5ec0cec08486df5f5ffbbfcdd08a6be07
Rakesh DR <rakeshdr543@gmail.com>
2021-03-05 06:02:27 +0530

    Added intial window

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hi electron')
+const {BrowserWindow,app} = require('electron')
+
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width:1200,
+        height:800,
+        backgroundColor:'white',
+        webPreferences:{
+            nodeIntegration:false,
+            worldSafeExecuteJavaScript:true,
+            contextIsolation:true,
+        }
+    })
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
